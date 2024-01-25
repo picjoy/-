4e76fb7465b9d9ef3aa27b8b998da04cfd86f1cd
JOY <piczo617@gmail.com>
2024-01-25 23:48:57 +0900

    Update 홀짝 구분하기.java

--- Coding Test/기초 트레이닝/홀짝 구분하기.java
+++ Coding Test/기초 트레이닝/홀짝 구분하기.java
@@ -5,10 +5,12 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        if(n % 2 == 0){
-            System.out.println(n+" is even");
-        } else {
-            System.out.println(n+" is odd");
-        }
+//         if(n % 2 == 0){
+//             System.out.println(n+" is even");
+//         } else {
+//             System.out.println(n+" is odd");
+//         }
+        // 삼항연산자
+        System.out.print(n + " is "+(n % 2 == 0 ? "even" : "odd"));
     }
 }
